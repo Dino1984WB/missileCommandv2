10d1aab246487ba457d49e008255246d19a27895
Dino1984 <WILliamkbukowksidev@gmail.com>
2023-12-28 03:17:38 -0500

    adding red missiles that explode in air

--- scriptindex.js
+++ scriptindex.js
@@ -6,19 +6,33 @@ document.addEventListener("DOMContentLoaded", function () {
         missileShooter.style.left = event.clientX + "px";
     });
 
-    // Create random missiles at regular intervals
-    setInterval(function () {
-        createRandomMissile();
-    }, 2000); // Adjust the firing interval
+    document.addEventListener("click", function (event) {
+        // Shoot a red missile when the mouse is clicked
+        createRedMissile(event.clientX);
+    });
 
-    function createRandomMissile() {
+    // Create blue missiles at regular intervals
+    setInterval(function () {
+        createBlueMissile();
+    }, 1000); // Adjust the firing interval
+
+    function createBlueMissile() {
         const randomX = Math.random() * window.innerWidth;
         const missile = document.createElement("div");
-        missile.className = "missile";
+        missile.className = "missile blue-missile";
         missile.style.left = randomX + "px";
         missile.style.top = "0px";
         document.body.appendChild(missile);
         animateMissile(missile);
+    }
+
+    function createRedMissile(initialX) {
+        const missile = document.createElement("div");
+        missile.className = "missile red-missile";
+        missile.style.left = initialX + "px";
+        missile.style.bottom = "20px"; // Adjust the starting position
+        document.body.appendChild(missile);
+        animateRedMissile(missile);
     }
 
     function animateMissile(missile) {
@@ -33,6 +47,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // If the missile reaches the bottom or collides, stop the animation
             if (position.y >= window.innerHeight) {
+                clearInterval(missileInterval);
+                document.body.removeChild(missile);
+            }
+        }, 20);
+    }
+
+    function animateRedMissile(missile) {
+        let position = { x: parseFloat(missile.style.left), y: parseFloat(missile.style.bottom) };
+
+        const missileInterval = setInterval(function () {
+            position.y += 5; // Adjust the speed of the missile
+            missile.style.bottom = position.y + "px";
+
+            // Check for collisions with cities or other game elements
+            // Implement your collision detection logic here
+
+            // If the missile reaches the top or collides, stop the animation
+            if (position.y >= window.innerHeight - 20) { // Adjust the ending position
                 clearInterval(missileInterval);
                 document.body.removeChild(missile);
             }
